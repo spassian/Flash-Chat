6333a10bcc9e260ab56b749160d9db8d48258302
Udit Singh <62090173+spassian@users.noreply.github.com>
2022-09-22 19:11:50 +0530

    Add files via upload

---
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -0,0 +1,24 @@
+
+import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseCore
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+
+    @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e)
+                } else {
+                    self.performSegue(withIdentifier: K.loginSegue, sender: self)
+                }
+            }
+        }
+    }
+}
